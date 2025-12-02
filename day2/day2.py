c7b7d6142a4791628a9b3c271de4437ee9b6b6bc
Kristoffer1122 <krkja001@osloskolen.no>
2025-12-02 19:49:59 +0100

    day2 part2 complete

--- day2/day2.py
+++ day2/day2.py
@@ -3,25 +3,27 @@ input = open("./input.txt", "r").read().strip()
 pos = 0
 sum = 0
 
-# if we find same two strings in range
-# find same characters in the range
-# add sum of false id
-
 for line in input.split(","):
-    newline = line.split("-");
-    seen = set()
+    newline = line. split("-")
 
     for id in range(int(newline[0]), int(newline[1])+1):
-
         strid = str(id)
-        splitid = list(strid)
-        lenid = round(len(splitid)/2)
-        print(splitid)
+        lenid = len(strid)
 
-        print("\ncomparing:", splitid[0:int(lenid)], "with", splitid[int(lenid):])
-        if splitid[0:int(lenid)] == splitid[int(lenid):]:
-            print("invalid id:", strid)
-            sum += id
+        # try all possible pattern lengths
+        for patternlen in range(1, int(lenid/2)+1):
+
+            # check if length is divisible by pattern length
+            if lenid % patternlen == 0:
+                pattern = strid[0:patternlen]
+                repetitions = int(lenid / patternlen)
+
+                print("\n",pattern, "repeated", repetitions, "times")
+
+                # check if pattern repeated makes the full id
+                if pattern * repetitions == strid:
+                    print("invalid id:", strid)
+                    sum += id
+                    break
 
 print("sum:",sum)
-
